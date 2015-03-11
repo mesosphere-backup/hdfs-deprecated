3493c7b332424a88bdd8ccb42a7f625c7f887e6a
Ken Sipe <kensipe@gmail.com>
2015-03-11 03:06:02 -0500

    removing unnecessary final modifier

--- src/main/java/org/apache/mesos/hdfs/util/HDFSConstants.java
+++ src/main/java/org/apache/mesos/hdfs/util/HDFSConstants.java
@@ -7,33 +7,33 @@ public interface HDFSConstants {
 
   // Total number of NameNodes
   // Note: We do not currently support more or less than 2 NameNodes
-  final Integer TOTAL_NAME_NODES = 2;
+  Integer TOTAL_NAME_NODES = 2;
 
   // Messages
-  final String NAME_NODE_INIT_MESSAGE = "-i";
-  final String NAME_NODE_BOOTSTRAP_MESSAGE = "-b";
-  final String RELOAD_CONFIG = "reload config";
+  String NAME_NODE_INIT_MESSAGE = "-i";
+  String NAME_NODE_BOOTSTRAP_MESSAGE = "-b";
+  String RELOAD_CONFIG = "reload config";
 
   // NodeIds
-  final String NAME_NODE_ID = "namenode";
-  final String JOURNAL_NODE_ID = "journalnode";
-  final String DATA_NODE_ID = "datanode";
-  final String ZKFC_NODE_ID = "zkfc";
+  String NAME_NODE_ID = "namenode";
+  String JOURNAL_NODE_ID = "journalnode";
+  String DATA_NODE_ID = "datanode";
+  String ZKFC_NODE_ID = "zkfc";
 
   // NameNode TaskId
-  final String NAME_NODE_TASKID = ".namenode.namenode.";
+  String NAME_NODE_TASKID = ".namenode.namenode.";
 
   // ExecutorsIds
-  final String NODE_EXECUTOR_ID = "NodeExecutor";
-  final String NAME_NODE_EXECUTOR_ID = "NameNodeExecutor";
+  String NODE_EXECUTOR_ID = "NodeExecutor";
+  String NAME_NODE_EXECUTOR_ID = "NameNodeExecutor";
 
   // Path to Store HDFS Binary
-  final String HDFS_BINARY_DIR = "hdfs";
+  String HDFS_BINARY_DIR = "hdfs";
 
   // Current HDFS Binary File Name
-  final String HDFS_BINARY_FILE_NAME = "hdfs-mesos-0.0.2.tgz";
+  String HDFS_BINARY_FILE_NAME = "hdfs-mesos-0.0.2.tgz";
 
   // HDFS Config File Name
-  final String HDFS_CONFIG_FILE_NAME = "hdfs-site.xml";
+  String HDFS_CONFIG_FILE_NAME = "hdfs-site.xml";
 
 }
