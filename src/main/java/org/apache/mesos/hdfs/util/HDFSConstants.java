65fe44d37eb1f5f840805cf29f27c93ac7890d96
Ken Sipe <kensipe@gmail.com>
2015-03-11 02:12:52 -0500

    redundant static keyword removed

--- src/main/java/org/apache/mesos/hdfs/util/HDFSConstants.java
+++ src/main/java/org/apache/mesos/hdfs/util/HDFSConstants.java
@@ -4,33 +4,33 @@ public interface HDFSConstants {
 
   // Total number of NameNodes
   // Note: We do not currently support more or less than 2 NameNodes
-  static final Integer TOTAL_NAME_NODES = 2;
+   final Integer TOTAL_NAME_NODES = 2;
 
   // Messages
-  static final String NAME_NODE_INIT_MESSAGE = "-i";
-  static final String NAME_NODE_BOOTSTRAP_MESSAGE = "-b";
-  static final String RELOAD_CONFIG = "reload config";
+   final String NAME_NODE_INIT_MESSAGE = "-i";
+   final String NAME_NODE_BOOTSTRAP_MESSAGE = "-b";
+   final String RELOAD_CONFIG = "reload config";
 
   // NodeIds
-  static final String NAME_NODE_ID = "namenode";
-  static final String JOURNAL_NODE_ID = "journalnode";
-  static final String DATA_NODE_ID = "datanode";
-  static final String ZKFC_NODE_ID = "zkfc";
+   final String NAME_NODE_ID = "namenode";
+   final String JOURNAL_NODE_ID = "journalnode";
+   final String DATA_NODE_ID = "datanode";
+   final String ZKFC_NODE_ID = "zkfc";
 
   // NameNode TaskId
-  static final String NAME_NODE_TASKID = ".namenode.namenode.";
+   final String NAME_NODE_TASKID = ".namenode.namenode.";
 
   // ExecutorsIds
-  static final String NODE_EXECUTOR_ID = "NodeExecutor";
-  static final String NAME_NODE_EXECUTOR_ID = "NameNodeExecutor";
+   final String NODE_EXECUTOR_ID = "NodeExecutor";
+   final String NAME_NODE_EXECUTOR_ID = "NameNodeExecutor";
 
   // Path to Store HDFS Binary
-  static final String HDFS_BINARY_DIR = "hdfs";
+   final String HDFS_BINARY_DIR = "hdfs";
 
   // Current HDFS Binary File Name
-  static final String HDFS_BINARY_FILE_NAME = "hdfs-mesos-0.0.2.tgz";
+   final String HDFS_BINARY_FILE_NAME = "hdfs-mesos-0.0.2.tgz";
 
   // HDFS Config File Name
-  static final String HDFS_CONFIG_FILE_NAME = "hdfs-site.xml";
+   final String HDFS_CONFIG_FILE_NAME = "hdfs-site.xml";
 
 }
