a950a21e66e10f3e3be6aa424803f1ca02082e3e
Ken Sipe <kensipe@gmail.com>
2015-03-16 10:07:07 -0600

    fixed runtime exception

--- src/main/java/org/apache/mesos/hdfs/config/ConfigServerException.java
+++ src/main/java/org/apache/mesos/hdfs/config/ConfigServerException.java
@@ -5,22 +5,11 @@ package org.apache.mesos.hdfs.config;
  */
 public class ConfigServerException extends RuntimeException {
 
-  private String message;
-
   public ConfigServerException(String msg) {
-    this.message = msg;
+    super(msg);
   }
 
   public ConfigServerException() {
-    this("");
-  }
-
-  @Override
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(String message) {
-    this.message = message;
+    super();
   }
 }
